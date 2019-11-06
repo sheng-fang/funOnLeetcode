2bafe039df3947836f8291017584c3417d6d6615
sheng <shengfang.ipeace@gmail.com>
2019-11-06 22:27:06 +0100

    modify name add gitignore .idea

---
+++ 7._Reverse_Integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        ele_list = []
+        if x ==0:
+            return 0
+        elif x < 0:
+            tmp_x = -x
+            flag = -1
+        else:
+            tmp_x = x
+            flag = 1
+
+        while tmp_x > 0:
+            ele_list.append(tmp_x % 10)
+            tmp_x = tmp_x // 10
+
+        reverse_val = 0
+        for idx, ele in enumerate(ele_list):
+            reverse_val += ele
+            reverse_val *= 10
+
+        return reverse_val * flag
