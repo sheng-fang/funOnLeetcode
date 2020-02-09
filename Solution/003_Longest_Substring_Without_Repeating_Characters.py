cc110ebd252226f211de803c8b934030b03f3a7f
sheng <shengfang.ipeace@gmail.com>
2020-02-09 22:27:11 +0100

    add 001_Two_Sum.py

---
+++ Solution/003_Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,32 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str):
+        assert isinstance(s, str)
+
+        if len(s) == 0:
+            return 0
+
+        if len(s) == 1:
+            return 1
+
+        rec = dict()
+        max_len = 0
+        head = 0
+        for idx, ltr in enumerate(s):
+            if ltr in rec:
+                curr_len = idx - head
+                head = rec[ltr] + 1
+                if curr_len > max_len:
+                    max_len = curr_len
+
+            rec[ltr] = idx
+
+        curr_len = len(s) - head
+        max_len = curr_len if curr_len > max_len else max_len
+
+        return max_len
+
+
+
+test_str = "abba"
+solu = Solution()
+print(solu.lengthOfLongestSubstring(test_str))
