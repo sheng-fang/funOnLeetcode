1a20cc80fde95b9269a5734a2a005a299ef6a295
sheng <shengfang.ipeace@gmail.com>
2020-02-20 23:15:16 +0100

    add routine trace to 064_Minimum_Path_Sum.py

--- Solution/064_Minimum_Path_Sum.py
+++ Solution/064_Minimum_Path_Sum.py
@@ -23,14 +23,29 @@ class Solution:
             raise Exception("Empty input!")
         n = len(grid[0])
 
-        dp = [[float('inf') for i in range(n+1)] for i in range(m+1)]
-        dp[0][1], dp[1][0] = 0, 0
+        dp = [0 for i in range(n)]
+        dp_rt = [[] for i in range(n)]
 
         for idx_m in range(0, m):
             for idx_n in range(0, n):
-                dp[idx_m + 1][idx_n + 1] = min(dp[idx_m][idx_n + 1], dp[idx_m + 1][idx_n]) + grid[idx_m][idx_n]
+                if idx_m == 0 and idx_n == 0:
+                    dp[0] = grid[idx_m][idx_n]
+                    dp_rt[0].append(idx_m * n + idx_n)
+                elif idx_m == 0:
+                    dp[idx_n] = dp[idx_n - 1] + grid[idx_m][idx_n]
+                    dp_rt[idx_n] = dp_rt[idx_n - 1] + [idx_m * n + idx_n]
+                elif idx_n == 0:
+                    dp[0] = dp[0] + grid[idx_m][idx_n]
+                    dp_rt[idx_n] = dp_rt[idx_n] + [idx_m * n + idx_n]
+                else:
+                    if dp[idx_n] > dp[idx_n - 1]:
+                        dp[idx_n] = dp[idx_n - 1] + grid[idx_m][idx_n]
+                        dp_rt[idx_n] = dp_rt[idx_n - 1] + [idx_m * n + idx_n]
+                    else:
+                        dp[idx_n] = dp[idx_n] + grid[idx_m][idx_n]
+                        dp_rt[idx_n] = dp_rt[idx_n] + [idx_m * n + idx_n]
 
-        return dp[m][n]
+        return dp[n - 1]
 
 
     def maxPathSum(self, grid: list):
